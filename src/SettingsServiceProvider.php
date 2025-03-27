c5640017039569c5bcae1dbacd8b1d3ea0ea76f0
xGrz <grzesiek.byskiniewicz@gmail.com>
2025-03-27 12:00:44 +0100

    Setup project

---
+++ src/SettingsServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace xGrz\Settings;
+
+use Illuminate\Support\ServiceProvider;
+
+class SettingsServiceProvider extends ServiceProvider
+{
+
+    public function register(): void
+    {
+    }
+
+    public function boot(): void
+    {
+
+    }
+
+}
