4add631da438f7cbf1424e569a38dcd6d07f5939
xGrz <grzesiek.byskiniewicz@gmail.com>
2025-03-31 10:35:47 +0200

    Added global settings helper

--- src/Helpers/global.php
+++ src/Helpers/global.php
@@ -1,1 +1,22 @@
 <?php
+
+use XGrz\Settings\Exceptions\SettingKeyNotFoundException;
+use XGrz\Settings\Facades\Settings;
+
+if (!function_exists('settings')) {
+
+    function settings(string $key, $default = null): int|float|bool|null|string
+    {
+        if (!is_null($default)) {
+            try {
+                $settingValue = Settings::get($key);
+            } catch (SettingKeyNotFoundException $ex) {
+                $settingValue = $default;
+            }
+            return $settingValue;
+        }
+        return Settings::get($key);
+
+    }
+
+}
---
+++ tests/Feature/GlobalHelperTest.php
@@ -0,0 +1,45 @@
+<?php
+
+
+use XGrz\Settings\Exceptions\SettingKeyNotFoundException;
+use XGrz\Settings\Facades\Settings;
+use XGrz\Settings\Helpers\InitBaseSettings;
+use XGrz\Settings\Models\Setting;
+use XGrz\Settings\Tests\TestCase;
+
+class GlobalHelperTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+        Setting::truncate();
+        InitBaseSettings::make();
+    }
+
+
+    public function test_global_helper_returns_setting_value(): void
+    {
+        Settings::refreshCache();
+        $this->expectsDatabaseQueryCount(0);
+
+        $this->assertSame(12, settings('system.name'));
+    }
+
+    public function test_global_helper_returns_default_value_when_settings_key_missing_and_default_value_is_set(): void
+    {
+        Settings::refreshCache();
+        $this->expectsDatabaseQueryCount(0);
+
+        $this->assertSame(50, settings('system.name1', 50));
+    }
+
+    public function test_global_helper_throws_exception_when_settings_key_missing_and_default_value_is_not_set(): void
+    {
+        Settings::refreshCache();
+        $this->expectsDatabaseQueryCount(0);
+        $this->expectException(SettingKeyNotFoundException::class);
+
+        $this->assertSame(50, settings('system.name1'));
+    }
+
+}
