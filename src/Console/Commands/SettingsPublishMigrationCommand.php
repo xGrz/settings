85c72ac8bb5757f9b4d6e3a12f48de43d54204ca
xGrz <grzesiek.byskiniewicz@gmail.com>
2025-04-10 13:03:40 +0200

    Add command to publish migration.

---
+++ src/Console/Commands/SettingsPublishMigrationCommand.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace XGrz\Settings\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class SettingsPublishMigrationCommand extends Command
+{
+    protected $signature = 'settings:publish-migration';
+
+    protected $description = 'Publishes migration from package';
+
+    public function handle(): int
+    {
+        $this->call('vendor:publish', ['--tag' => 'settings-migrations']);
+        return 0;
+    }
+}
